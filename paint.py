5a06a4fc84b75d6a9f6dc9d18f4540782dd60e18
TwiliShiba <twilishiba@gmail.com>
2021-05-04 22:36:15 -0500

    Tere Angulo. Mejora de función circ, creación de rectangle, triangle y comentarios al código

--- paint.py
+++ paint.py
@@ -1,8 +1,10 @@
 from turtle import *
 from freegames import vector
+import math
 
 def line(start, end):
     "Draw line from start to end."
+    "Crea una línea desde el punto de inicio hasta el punto final"
     up()
     goto(start.x, start.y)
     down()
@@ -10,11 +12,13 @@ def line(start, end):
 
 def square(start, end):
     "Draw square from start to end."
+    "Crea un cuadrado con una esquina inferior izquierda en el punto de inicio"
+    "con con una esquina inferior derecha en el punto final"
     up()
     goto(start.x, start.y)
     down()
     begin_fill()
-
+    "Ciclo para repetir el movimiento hacia delante y la rotación de 90° a la izquierda cuatro veces"
     for count in range(4):
         forward(end.x - start.x)
         left(90)
@@ -23,22 +27,54 @@ def square(start, end):
 
 def circ(start, end):
     "Draw circle from start to end."
+    "Crea un círculo con un centro en el punto de inicio y un radio en punto final"
+    
+    "Variable que calcula la distancia entre punto de inicio (centro) y"
+    "el radio en punto final"
+    rad = math.sqrt((end.x - start.x)**2 + (end.y - start.y)**2)
+    "Variable que guarda el centro del círculo"
+    center = start.y - rad
+    
+    up()
+    goto(start.x, center)
+    down()
+    begin_fill()
+    "Dibujar el círculo"
+    circle(rad)
+
+    end_fill()
+
+def rectangle(start, end):
+    "Draw rectangle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    
+    "Ciclo para repetir el movimiento hacia delante, la rotación de 90° a la izquierda,"
+    "el movimiento hacia delante dos veces menos que el lado horizontal y otra rotación"
+    "de 90° a la izquierda 2 veces"
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward((end.x - start.x) / 2)
+        left(90)
+
+    end_fill()
+
+def triangle(start, end):
+    "Draw triangle from start to end."
     up()
     goto(start.x, start.y)
     down()
     begin_fill()
 
-    rad = end.x - start.x
-    circle(rad)
+    "Ciclo para repetir el movimiento hacia delante y la rotación de 120° a la izquierda tres veces"
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+
     end_fill()
-
-def rectangle(start, end):
-    "Draw rectangle from start to end."
-    pass  # TODO
-
-def triangle(start, end):
-    "Draw triangle from start to end."
-    pass  # TODO
 
 def tap(x, y):
     "Store starting point or draw shape."
